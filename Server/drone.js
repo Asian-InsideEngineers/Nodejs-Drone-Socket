c9566e9ebb26475e386db9d30ce85cb23453f959
Research In Flying Motion <insideengineers2611@gmail.com>
2024-05-26 13:44:45 +0530

    Updated Code

--- Server/drone.js
+++ Server/drone.js
@@ -1,13 +1,13 @@
 const express = require('express');
 const WebSocket = require('ws');
-const Gpio = require('onoff').Gpio; // Import the onoff library for GPIO control
+// const Gpio = require('onoff').Gpio; // Import the onoff library for GPIO control
 
 const app = express();
 const server = require('http').createServer(app);
 const wss = new WebSocket.Server({ server });
 
 // Setup GPIO pin, assuming GPIO 17 is used for example
-const led = new Gpio(17, 'out'); // Change the pin number as needed
+// const led = new Gpio(17, 'out'); // Change the pin number as needed
 
 app.use(express.json());
 
@@ -28,18 +28,18 @@ wss.on('connection', function connection(ws) {
     if (message.toString() === 'ConnectDrone') {
       // Execute GPIO operation: Turn on the LED
       try {
-        led.writeSync(1); // Set GPIO pin high
+        // led.writeSync(1); // Set GPIO pin highcle
         ws.send('Drone Server Connected');
         console.log('Sent response to Flutter: Drone Server Connected');
         
         // Optionally, turn off the LED after a certain time (e.g., 5 seconds)
-        setTimeout(() => {
-          try {
-            led.writeSync(0); // Set GPIO pin low
-          } catch (err) {
-            console.error('Failed to turn off the LED:', err);
-          }
-        }, 5000);
+        // setTimeout(() => {
+        //   try {
+        //     led.writeSync(0); // Set GPIO pin low
+        //   } catch (err) {
+        //     console.error('Failed to turn off the LED:', err);
+        //   }
+        // }, 5000);
       } catch (err) {
         console.error('Failed to turn on the LED:', err);
         ws.send('Error: Failed to connect to the drone');
@@ -49,18 +49,18 @@ wss.on('connection', function connection(ws) {
 });
 
 // Clean up GPIO state on exit
-const cleanUp = () => {
-  try {
-    led.writeSync(0); // Turn off the LED
-    led.unexport(); // Unexport the GPIO pin
-  } catch (err) {
-    console.error('Failed to clean up GPIO:', err);
-  }
-  process.exit();
-};
+// const cleanUp = () => {
+//   try {
+//     led.writeSync(0); // Turn off the LED
+//     led.unexport(); // Unexport the GPIO pin
+//   } catch (err) {
+//     console.error('Failed to clean up GPIO:', err);
+//   }
+//   process.exit();
+// };
 
-process.on('SIGINT', cleanUp); // Handle Ctrl+C
-process.on('SIGTERM', cleanUp); // Handle termination signal
+// process.on('SIGINT', cleanUp); // Handle Ctrl+C
+// process.on('SIGTERM', cleanUp); // Handle termination signal
 
 server.listen(8080, () => {
   console.log('Server started on port 8080');
